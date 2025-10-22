9a1005dad473d1366f80da66d262c8a0d507eb6a
shuxl <985776854@qq.com>
2025-10-22 15:25:21 +0800

    nacos接入 完成

--- agent/mcpServer/src/main/java/com/lance/McpServerApp.java
+++ agent/mcpServer/src/main/java/com/lance/McpServerApp.java
@@ -7,7 +7,7 @@ import org.springframework.ai.tool.function.FunctionToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -19,9 +19,9 @@ import org.springframework.context.annotation.Import;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {
-        "com.lance.service.feign.**"
-})
+//@EnableFeignClients(basePackages = {
+//        "com.lance.service.feign.**"
+//})
 @Import(McpToolConfiguration.class)
 public class McpServerApp {
     
